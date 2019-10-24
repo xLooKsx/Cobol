e782c11033939c015edb742dec8b85a0f38e3ab9
Lucas Oliveira <lu.oliveira.silva98@gmail.com>
2019-10-23 21:02:05 -0300

    Comandos Basicos
    Nesses arquivos foram inseridos o header basico que serão usados nos
    arquivos, foi mostrado o classico 'hello world', alem de mostra a
    captura de dados apartir do que foi digitado no console e aplicação de
    mascara para formatar numero

---
+++ HelloWorld.cob
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Hello_World.
+      *********************************************
+      ***    AREA DE COMENTARIOS
+      ***    OBJETIVO DO PROGRAMA: MOSTRAR O HELLO WORLD
+      ***    AUTHOR: LUCAS OLIVEIRA(LooKs)
+      ***    DATA: 23/10/2019
+      *********************************************
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+          DISPLAY 'HELLO WORLD'.
+          STOP RUN.
